86ba40e290d5b39febee0f31a3e3ad8cec16ec98
doanpts <doanpts@gmail.com>
2015-06-30 07:28:05 +0700

    sua doi ngay 30/6

--- connect.php
+++ connect.php
@@ -2,8 +2,10 @@
 	$host = 'localhost';
 	$user = 'root';
 	$pass = '';
-	$db = 'doantran';
+	$db = 'inews';
 	$connect = mysql_connect($host,$user,$pass);
 	$select = mysql_select_db($db,$connect);
 	$lang = mysql_query("SET NAMES 'utf8'");
+	// nothing to do
+	echo 'hello!';
 ?>
